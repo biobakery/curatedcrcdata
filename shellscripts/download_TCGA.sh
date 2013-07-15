37881ee25883e549dc55865a15fabeadff14eb51
Princy Parsana <princyparsana@bitbucket.org>
2013-07-15 09:51:49 -0400

    updating curation scripts

--- shellscripts/download_TCGA.sh
+++ shellscripts/download_TCGA.sh
@@ -23,4 +23,6 @@ wget --timestamping $URL/unc.edu_COAD.AgilentG4502A_07_3.Level_3.1.5.0.tar.gz
 wget --timestamping $URL/unc.edu_COAD.AgilentG4502A_07_3.Level_3.2.0.0.tar.gz
 
 for file in `ls *.tar.gz`;do tar xfz $file;done
-
+find . -name "*tcga_level3.data.txt" > TCGA_file_sources.txt
+for file in `find . -name "*tcga_level3.data.txt"`;do mv $file .;done
+rm -f *.tar.gz
